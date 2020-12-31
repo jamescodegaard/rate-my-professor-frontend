6fc070fbde33de79c897a12ce1fbdf1e1c5d66d4
Becca <rrdoukas@gmail.com>
2020-12-31 13:40:58 -0600

    adds routes and professor show/index

---
+++ src/views/ProfessorsIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="professors-index">
+    <div v-for="professor in professors">
+      <h1>{{ professor.first_name }} {{ professor.last_name }}</h1>
+      <h3>{{ professor.school }}</h3>
+      <h3>{{ professor.department }}</h3>
+      <h3>{{ professor.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      professors: {},
+      error: [],
+    };
+  },
+  created: function() {
+    axios
+      .get("/professors")
+      .then((response) => (this.professors = response.data));
+  },
+  methods: {},
+};
+</script>
